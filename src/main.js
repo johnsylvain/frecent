3c9514512a06a4ac7eb6d9f7f003384663443b22
John Sylvain <john.n.sylvain@gmail.com>
2017-11-14 19:20:01 -0600

    refactored api scheme

--- src/main.js
+++ src/main.js
@@ -15,7 +15,9 @@ export default (function () {
       return Math.round(Math.abs((a.getTime() - b.getTime()) / (oneDay)));
     }
 
-    return (visits * 100) / getDays(new Date(), timestamp)
+    const days = getDays(new Date(), timestamp)
+
+    return (visits * 100) / ((days === 0) ? 1 : days)
   }
 
   Frecent.prototype.get = function get() {
@@ -24,20 +26,34 @@ export default (function () {
         item,
         item._weight = this.frecency(item._visits, item._lastVisit)
       ))
-      .sort((a, b) => a._weight >= b._weight)
+      .sort((a, b) => a._weight <= b._weight)
   }
 
   Frecent.prototype.load = function load(items) {
-    this.items = items.map(item => ({
-      item,
-      _visits: 0,
-      _lastVisit: null,
-      _weight: null
-    }))
+    const predicate = item => {
+      if (!item.body) {
+        return {
+          body: item,
+          _visits: 0,
+          _lastVisit: null,
+          _weight: null
+        }
+      }
+      return item
+    }
+
+    this.items = items.map(predicate)
   }
 
-  Frecent.prototype.visit = function visit(item) {
-    let ref = this.items.find(i => i.item === item)
+  Frecent.prototype.visit = function visit(key, item, cb) {
+    function prop(obj,path) {
+      path=path.split('.');
+      var res=obj;
+      for (var i=0;i<path.length;i++) res=res[path[i]];
+      return res;
+    }
+
+    let ref = this.items.find(i => prop(i.body, key) === item)
     let idx = this.items.indexOf(ref);
 
     Object.assign(this.items[idx], {
@@ -45,6 +61,7 @@ export default (function () {
       _lastVisit: new Date()
     })
 
+    if (typeof cb !== 'undefined') cb()
   }
 
   return Frecent
