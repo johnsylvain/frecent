651f3467d27a2e7e85742cc55cb5ce9d157667cc
John Sylvain <john.n.sylvain@gmail.com>
2018-01-20 08:47:35 -0600

    Transpile down to es5 (#5)

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,35 @@
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
+import babel from 'rollup-plugin-babel'
 import pkg from './package.json'
 
-export default {
-	input: pkg.source,
-	external: ['ms'],
-	output: [
-		{ file: pkg.main, format: 'cjs' },
-		{ file: pkg.module, format: 'es' },
-		{ file: pkg.browser, format: 'umd' }
-	],
-	name: 'frecent',
-	plugins: [
-		resolve(),
-		commonjs()
-	]
-}
+export default [
+	{
+		input: pkg.source,
+		external: ['ms'],
+		output: [
+			{ file: pkg.main, format: 'cjs' },
+			{ file: pkg.browser, format: 'umd' }
+		],
+		name: 'frecent',
+		plugins: [
+			resolve(),
+			commonjs(),
+			babel({
+				exclude: 'node_modules/**'
+			})
+		]
+	},
+	{
+		input: pkg.source,
+		external: ['ms'],
+		output: [
+			{ file: pkg.module, format: 'es' }
+		],
+		name: 'frecent',
+		plugins: [
+			resolve(),
+			commonjs()
+		]
+	}
+]
